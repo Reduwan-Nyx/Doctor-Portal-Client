d199dd7e7f20fee6645abfe38a9690fb7ed14797
Tahsim Reduwan Naim <88936596+Reduwan-Nyx@users.noreply.github.com>
2022-08-06 21:50:47 +0600

    available appointment start

---
+++ src/Pages/Appointment/AppointmentBanner.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { DayPicker } from "react-day-picker";
+import chair from '../../assets/images/chair.png'
+import "react-day-picker/dist/style.css";
+import './AppointmentBanner.css'
+import back from '../../assets/images/cool-background.png'
+const AppointmentBanner = ({date, setDate}) => {
+    
+  return (
+    <div style={{
+        background: `url(${back})`,
+        backgroundSize: 'cover'
+    }} class="hero min-h-screen ">
+      <div class="hero-content flex-col lg:flex-row-reverse ">
+        <img
+          src={chair}
+          class="max-w-sm rounded-lg shadow-2xl"
+       alt="Dentist chair" />
+        <div>
+        <DayPicker mode="single" selected={date} onSelect={setDate} />
+       
+        </div>
+      </div>
+     
+    </div>
+  );
+};
+
+export default AppointmentBanner;
