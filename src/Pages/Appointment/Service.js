5572124320e43b78d6215e22c4f023faa66f61f6
Reduwan-Nyx <88936596+Reduwan-Nyx@users.noreply.github.com>
2022-08-07 19:27:09 +0600

    service addedd

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -8,7 +8,7 @@ const Service = ({ service }) => {
         <h2 class="text-center uppercase font-bold text-xl font-mono">{name}</h2>
         <p className="font-mono">{
             slots.length > 0 ? <span>{slots[0]}</span>
-            : <span className="text-red-500">Try Another Date</span>
+            : <span className="text-red-500">Not Available <br /> Try Another Date </span>
             }</p>
         <p className="font-mono">{slots.length} {slots.length > 1 ? 'Spaces' : 'Space'} Available</p>
         <div class="card-actions justify-center">
